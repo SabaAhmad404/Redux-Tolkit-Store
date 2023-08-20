571ce192e76be593acd74d37b59a534d72792f5e
SabaAhmad404 <sabaahmad895@gmail.com>
2023-08-20 22:43:14 +0500

    redux store

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Cart from './components/Cart';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import './App.css';
+import store from './components/store/store';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route exact path="/" Component={Home} />
-        <Route path="cart" Component={Cart} />
-      </Routes>
+      <Provider store={store}>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route path="cart" Component={Cart} />
+        </Routes>
+      </Provider>
     </div>
+
   );
 }
 
